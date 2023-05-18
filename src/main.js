1ce408d582e4ccaf19035e5c0de30d72b1487e25
Patricia Atanasoff <system.informatica365@gmail.com>
2023-05-18 03:10:01 -0300

    header and hero section finished

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
+//Navbar open-close
 $(document).ready(function(){
     $('#navbar-show-btn').click(() => $('.navbar-box').removeClass('translate-x-full'));
     $('#navbar-hide-btn').click(() => $('.navbar-box').addClass('translate-x-full'));
+
+    //Date picker
+    $('[data-toggle="datepicker"]').datepicker();
+
 })
+
